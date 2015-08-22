5108e03d5d2d2f528ab4b0ebac162578ee662a50
Joel Kuiper <me@joelkuiper.eu>
2015-08-22 21:15:30 +0200

    Rename to yesparql.jena to yesparql.tdb

--- src/yesparql/tdb.clj
+++ src/yesparql/tdb.clj
@@ -1,19 +1,25 @@
-(ns yesparql.jena
+(ns yesparql.tdb
   (:import
    [org.apache.jena.query Dataset DatasetFactory]
    [org.apache.jena.tdb TDBFactory TDBLoader StoreConnection TDB]))
 
-(defn ^Dataset create-file-tdb
+(defn ^Dataset create-file-based
   "Creates a new TDB-backed `Dataset` in the `directory` (absolute path)"
   [^String directory]
   (TDBFactory/createDataset directory))
 
-(defn ^Dataset create-assembled-tdb
+(defn ^Dataset create-assembler-based
   "Creates a new TDB-backed `Dataset` with the provided assembler file (TTL, absolute path)"
   [assembler]
   (TDBFactory/assembleDataset assembler))
 
-(defn ^Dataset create-in-mem-tdb
-  "Create an in-memory, modifiable `Dataset`"
+(defn ^Dataset create-in-memory
+  "Create an in-memory, modifiable TDB `Dataset`"
   []
   (TDBFactory/createDataset))
+
+(defn ^Dataset create-bare
+  "Creates a bare `DataSet` (without TDB)
+   Recommended for testing purposes only"
+  []
+  (DatasetFactory/createMem))
