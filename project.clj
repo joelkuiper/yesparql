8fac5c0b1f9c058197b37793e20d2620f08f7e43
Joel Kuiper <me@joelkuiper.eu>
2015-09-01 12:56:46 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/joelkuiper/yesparql"}
-  :profiles {:dev {:dependencies [[expectations "2.1.2"]
+  :profiles {:dev {:dependencies [[expectations "2.1.3"]
                                   [org.apache.derby/derby "10.11.1.1"]]
                    :plugins [[lein-autoexpect "1.4.0"]
                              [lein-expectations "0.0.8"]]}
@@ -14,7 +14,7 @@
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0-alpha4"]]}}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [instaparse "1.3.6"]
+                 [instaparse "1.4.1"]
 
                  [org.apache.jena/jena-arq "3.0.0"]
                  [org.apache.jena/jena-text "3.0.0"]
