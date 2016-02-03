9ad8c30e919a8b0b9b8aafc49211a5ebed0c697d
Joel Kuiper <me@joelkuiper.eu>
2016-02-03 14:16:01 +0100

    Clojure 1.8.0

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [instaparse "1.4.1"]
 
                  [org.apache.jena/jena-arq "3.0.1"]
